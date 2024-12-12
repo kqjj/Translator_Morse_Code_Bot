3c002ed118d9ca0ff3c773f656fda97418730c12
kqjj <kqjj4369@gmail.com>
2024-12-12 18:53:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-TOKEN = '...' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
+TOKEN = '7376682624:AAFuZXujuqAtJaMB8jdZRTCHif8RpwbcH3Y' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
